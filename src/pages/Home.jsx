bea9ad3a5f7573e539ec50164506b7bfbedeaee2
Emanuel Maldonado Tuero <ema14mt@gmail.com>
2025-07-22 20:43:43 +0200

    feat: upload image + OCR endpoint called

---
+++ src/pages/Home.jsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = "K89732725588957";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [base64IMG, setBase64IMG] = useState();
+
+  function uploadImage(event) {
+    // const img = URL.createObjectURL(event.target.files[0]);
+    const image = event.target.files[0];
+    convertToBase64(image);
+  }
+
+  const convertToBase64 = (img) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(img);
+
+    reader.onload = () => {
+      // console.log("called: ", reader);
+      setBase64IMG(reader.result);
+    };
+    reader.onerror = (error) => {
+      console.error("Error converting image to base64:", error);
+    };
+  };
+
+  const recognizeText = async () => {
+    try {
+      setLoading(true);
+
+      const body = `base64Image=${encodeURIComponent(
+        base64IMG
+      )}&language=spa&isTable=true&OCREngine=2`;
+
+      // Petición a la API de OCR
+      const url = "https://api.ocr.space/parse/image";
+      const headers = {
+        apikey: API_KEY,
+        "Content-Type": "application/x-www-form-urlencoded",
+      };
+
+      const response = await axios.post(url, body, { headers });
+
+      if (response.status !== 200) {
+        throw new Error("Error en la solicitud a la API de OCR");
+      }
+
+      console.log("response.data -----> ", response.data);
+      // const data = await response.json();
+
+      // if (!data.IsErroredOnProcessing) {
+      //   const parsedText = data.ParsedResults?.[0]?.ParsedText || "";
+      //   console.log("parsedText -----> ", parsedText);
+      // } else {
+      //   alert("Error procesando la imagen");
+      //   console.log(data);
+      // }
+    } catch (error) {
+      console.error("Error reconociendo texto:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    recognizeText();
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-4">Ticket to Excel</h1>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+        <input
+          className="border-2 py-2 px-4 rounded-4xl cursor-pointer"
+          type="file"
+          onChange={uploadImage}
+        />
+
+        {loading && <div className="text-center text-blue-500">Loading...</div>}
+
+        {base64IMG && (
+          <>
+            <div className="w-[500px] h-[500px] my-4 border-2 border-dashed">
+              <img
+                className="w-full h-full object-contain"
+                src={base64IMG}
+                alt="Uploaded image"
+              />
+            </div>
+            <button
+              className="border-2 py-2 px-4 rounded-4xl cursor-pointer"
+              type="submit"
+            >
+              Convert to Excel
+            </button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
